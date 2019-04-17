b73f0bb50dd2ec067f443baccf34ed5a25dbdbf1
Hu Bin <940925304@qq.com>
2019-04-17 10:11:19 +0800

    1

---
+++ Atoi/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string str = Console.ReadLine();
+            Console.WriteLine(p.MyAtoi(str));
+        }
+
+        char[] cs    = null;
+        int len      = 0;
+        int pnt      = 0;
+        long result  = 0;
+        int neg      = 1;
+
+        public int MyAtoi(String str)
+        {
+            if (str == null || str.Length == 0)
+                return 0;
+
+            cs = str.ToCharArray();
+            len = str.Length;
+            pnt = 0;
+            result = 0;
+
+            ProcessSpace();
+
+            if (!ProcessMinus())
+                return 0;
+            if (!ProcessNumber())
+                return 0;
+
+            int ret = GetResult();
+
+            return ret;
+        }
+        private bool ProcessSpace()
+        {
+            while (pnt < len && cs[pnt] == ' ')
+                pnt++;
+            return true;
+        }
+
+        private bool ProcessMinus()
+        {
+            if (pnt >= len)
+                return false;
+
+            if (cs[pnt] == '-')
+            {
+                neg = -1;
+                pnt++;
+            }
+            else if (cs[pnt] == '+')
+            {
+                neg = 1;
+                pnt++;
+            }
+
+            return true;
+        }
+
+        private bool ProcessNumber()
+        {
+            if (pnt >= len)
+                return false;
+            
+            if (cs[pnt] < '0' || cs[pnt] > '9') return false;
+
+            while (pnt < len && (cs[pnt] >= '0' && cs[pnt] <= '9'))
+            {
+                int k = cs[pnt] - '0';
+                result = result * 10 + k;
+                long r = result * neg;
+                if (r > Int32.MaxValue || r < Int32.MinValue)
+                {
+                    return true;
+                }
+                pnt++;
+            }
+
+            return true;
+        }
+        private int GetResult()
+        {
+            long r = result * neg;
+            if (r > Int32.MaxValue)
+                return Int32.MaxValue;
+            if (r < Int32.MinValue)
+                return Int32.MinValue;
+
+            return (int)r;
+        }
+
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arry = { 1,3,4,7,8,9,12,13,15,18};
+
+            Program p = new Program();
+            Console.WriteLine(p.BinarySearch(arry,3));
+            Console.WriteLine(p.BinarySearchRecursion(arry,0,arry.Length-1, 7));
+            
+        }
+
+        public int BinarySearch(int[] arry,int value)
+        {
+            //排序
+
+            int left = 0;
+            int right = arry.Length - 1;
+
+            while(left <= right)
+            {
+                int mid = (right + left) / 2;
+                if(arry[mid] < value)
+                {
+                    left = mid + 1;
+                }
+                else if(arry[mid] > value)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    return mid + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        //递归
+        public int BinarySearchRecursion(int[] arry, int left, int right, int value)
+        {
+            //排序            
+
+            if(left <= right)
+            {
+                int mid = (right + left) / 2;
+                if (arry[mid] < value)
+                {
+                    left = mid + 1;
+                    return BinarySearchRecursion(arry, left, right, value);
+                }
+                else if (arry[mid] > value)
+                {
+                    right = mid - 1;
+                    return BinarySearchRecursion(arry, left, right, value);
+                }
+                else
+                {
+                    return mid + 1;
+                }
+            }
+         
+            return -1;
+        }
+    }
+}
